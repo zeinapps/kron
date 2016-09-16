a7817fceb4dea8c4aaf18b5afeb832fad3267500
zein <zein@jayantara.co.id>
2016-09-16 14:15:22 -0700

    okzone title

--- ebarca_bola.okezone.com_index.php
+++ ebarca_bola.okezone.com_index.php
@@ -15,12 +15,12 @@ $ul = $html->find('ul[class=list-berita]', 0);
 foreach ($ul->find('li[class=col-md-4]') as $li) {
     //           echo $li->find('a',0);
     $url = trim($li->find('a', 0)->href);
-    $title = trim($li->find('h2', 0)->plaintext);
+    $title = trim($li->find('a', 1)->plaintext);
     $property = 'data-original';
     $img_tumb = trim($li->find('div[class=thumb-news]', 0)->$property);
     $sumber = 'bola.okezone.com';
     //            var_dump($url.' '.$title.' '.$img_tumb.' '.$sumber.' ');die;
-    $sql = "INSERT INTO ebarca_listurl (url,title,img_tumb,sumber) VALUES ('$url','$title','$img_tumb','$sumber')";
+     $sql = "INSERT INTO ebarca_listurl (url,title,img_tumb,sumber) VALUES ('$url','$title','$img_tumb','$sumber')";
     $conn->query($sql);
 }
 
